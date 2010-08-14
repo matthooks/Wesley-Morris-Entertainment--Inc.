f894ab2ff890f350e2e4f516452499b6e6295566
matthooks <matthooks@gmail.com>
2010-08-14 13:40:50 -0500

    turned caching back on

--- wesleymorrisentertainment.rb
+++ wesleymorrisentertainment.rb
@@ -1,9 +1,9 @@
 require 'erb'
 
-# before do
-#   headers "Content-Type" => "text/html; charset=utf-8"
-#   headers "Cache-Control" => "public, max-age=300"
-# end
+before do
+  headers "Content-Type" => "text/html; charset=utf-8"
+  headers "Cache-Control" => "public, max-age=300"
+end
 
 get '/' do
   erb :index
