330efe2652eb584fc8e17e86e64a6f3fc7f1826f
matthooks <matthooks@gmail.com>
2010-08-14 21:40:12 -0500

    yea

---
+++ public/javascripts/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+
+  var c = $('#home .container').children('div');
+  
+  $(c.get(0)).delay(500).show('scale');
+  $(c.get(1)).delay(1000).show('scale');
+  $(c.get(2)).delay(1500).show('scale');
+  
+});
